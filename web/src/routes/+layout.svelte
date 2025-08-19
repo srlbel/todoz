d56cb0c58faf666adcd64fc852eac89719918a90
Juan Simancas <dev.juansimancas@proton.me>
2025-08-18 19:49:09 -0500

    add biome rules and run linter

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
-	
-	let { children } = $props();
+import "../app.css";
+import favicon from "$lib/assets/favicon.svg";
+
+let { children } = $props();
 </script>
 
 <svelte:head>
